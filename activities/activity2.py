481b42a2666cb738584c1681854be68c6e9bb9db
Vien Calderon <viencalderon15@gmail.com>
2026-09-08 11:13:20 +0800

    activity 2

--- activities/activity2.py
+++ activities/activity2.py
@@ -4,6 +4,7 @@
 '''
 
 import threading
+import sys
 
 balance = 0
 
@@ -11,9 +12,33 @@ def update_balance():
     global balance
 
     for _ in range(100000):
-        balance += 1  # NOT ATOMIC — this is the critical region
+        temp = balance
+        sys.setswitchinterval(0.001)
+        balance = temp + 1
+        # balance += 1  # NOT ATOMIC — this is the critical region
 
-# TODO 1: Create two threads targeting update_balance()
-# TODO 2: Start both threads
-# TODO 3: Join both threads
-# TODO 4: Print the final balance and compare it to the expected value (200000)
+def run():
+    global balance
+    balance = 0
+
+    # TODO 1: Create two threads targeting update_balance()
+    # TODO 2: Start both threads
+    # TODO 3: Join both threads
+    # TODO 4: Print the final balance and compare it to the expected value (200000)
+
+    first_thread = threading.Thread(target = update_balance)
+    second_thread = threading.Thread(target = update_balance)
+
+    first_thread.start()
+    second_thread.start()
+
+    first_thread.join()
+    second_thread.join()
+
+    print(f"Expected Value: 200000\nFinal balance: {balance}")
+
+if __name__ == "__main__":
+    for i in range(1, 4):
+        print(f"Test: {i}")
+        run()
+        print("")
