3627e4ba630f210f81a18cd585fdaed8b9bd5065
Vien Calderon <viencalderon15@gmail.com>
2026-09-08 11:38:32 +0800

    activity 6

--- activities/activity6.py
+++ activities/activity6.py
@@ -13,6 +13,7 @@ def round_robin(processes, quantum):
     remaining = dict(processes)
     time_elapsed = 0
     order = []
+    finish_times = {}
 
     # TODO: Implement the round-robin loop:
     # - Dequeue a process
@@ -20,7 +21,44 @@ def round_robin(processes, quantum):
     # - If it still has remaining time, re-enqueue it
     # - Record execution order and finish times
 
-    return order
+    while queue:
+        pid, _ = queue.pop(0)
 
-processes = [("P1", 10), ("P2", 5), ("P3", 8)]
-print(round_robin(processes, quantum=3))
+        run_time = min(quantum, remaining[pid])
+        time_elapsed += run_time
+        remaining[pid] -= run_time
+
+        order.append((pid, run_time, time_elapsed))
+
+        if remaining[pid] > 0:
+            queue.append((pid, remaining[pid]))
+        else:
+            finish_times[pid] = time_elapsed
+
+    return order, finish_times
+
+def run():
+    processes = [("P1", 10), ("P2", 5), ("P3", 8)]
+    # print(round_robin(processes, quantum=3))
+
+    result, finish_times = round_robin(processes, quantum=3)
+    print("Execution order (pid, ran_for, time_now): ")
+
+    for entry in result:
+        print(entry)
+
+    print("\nFinish times:", finish_times)
+    total_wait = 0
+    burst_lookup = dict(processes)
+
+    for pid, finish in finish_times.items():
+        wait = finish - burst_lookup[pid]
+        total_wait += wait
+        print(f"{pid} waiting time: {wait}")
+
+    avg_wait = total_wait / len(processes)
+    print(f"\nAverage waiting time: {avg_wait}")
+
+if __name__ == "__main__":
+    run()
+
