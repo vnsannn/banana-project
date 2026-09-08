d7d498c55fa9d742b893e7a8eb994466fb9d1617
Vien Calderon <viencalderon15@gmail.com>
2026-09-08 11:26:56 +0800

    activity 3

--- activities/activity3.py
+++ activities/activity3.py
@@ -4,29 +4,59 @@
 '''
 
 import threading
-import time
+import sys
 
-lock_flag = 0                                # 0 = free, 1 = busy
+lock_flag = [0]                                # 0 = free, 1 = busy
 balance = 0
+flag_check = threading.Lock()
 
 def test_and_set(flag_list):
     # TODO: In ONE "atomic" step (simulate with threading.Lock only for the
     #   flag check itself), test if flag_list[0] == 0, and if so set it to 1.
     #   Return the OLD value.
-    pass
+    
+    with flag_check:
+        old_value = flag_list[0]
+        flag_list[0] = 1
+
+    return old_value
 
 def acquire_lock():
-    while test_and_set(...):                # busy waiting (spinning)
+    while test_and_set(lock_flag) == 1:                # busy waiting (spinning)
         pass
 
 def release_lock():
     global lock_flag
-    lock_flag = 0
+    lock_flag[0] = 0
 
 def update_balance_safe():
     global balance
 
     for _ in range(100000):
             acquire_lock()
-            balance += 1
+            temp = balance
+            sys.setswitchinterval(0.001)    
+            balance = temp + 1
+            # balance += 1
             release_lock()
+
+def run():
+    global balance
+    balance = 0
+
+    first_thread = threading.Thread(target = update_balance_safe)
+    second_thread = threading.Thread(target = update_balance_safe)
+    
+    first_thread.start()
+    second_thread.start()
+
+    first_thread.join()
+    second_thread.join()
+
+    print(f"Expected Value: 200000\nFinal balance: {balance}")
+
+if __name__ == "__main__":
+    for i in range(1, 4):
+        print(f"Test: {i}")
+        run()
+        print("")
